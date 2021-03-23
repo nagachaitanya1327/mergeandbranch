71677ec4ba19dc75e196596cc87a0477ae32ac46
HP <HP@NagaChaitanya>
2021-03-23 15:20:00 +0530

    first commit

---
+++ mergrandBranch/src/mergrandBranch/Test1.java
@@ -0,0 +1,20 @@
+package mergrandBranch;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		
+					int i=10;
+					System.out.println(i);
+					System.out.println("i");
+
+				System.out.println(10+5);
+				System.out.println("10+5");
+			}
+
+			
+
+	}
+
+
